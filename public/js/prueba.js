5748a5891c4f851c51a2d14019afda0e83453246
codigoenlaweb <codeenlaweb@gmail.com>
2022-01-24 11:03:31 -0400

    login css

---
+++ public/js/prueba.js
@@ -0,0 +1,19 @@
+const colors = document.querySelectorAll('.bg-principal-green');
+const color = document.querySelector('.bg-principal-green');
+const btn = document.querySelector('.btn');
+
+change = () => {
+    document.querySelector("html").classList.add("dark")
+}
+
+
+btn.addEventListener('click', change);
+
+
+
+// colors.forEach((color => {
+//     document.documentElement.style.
+// }))
+
+// color.classList
+
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'Quicksand': ['Quicksand', 'sans-serif'],
+        'Roboto': ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        'principal-green': '#ACD9B2',
+        'dark': '#232830',
+        'input-field': '#F7F7F7',
+        'secondary-gray': '#C7C7C7',
+      }
+    },
+  },
+  plugins: [],
+}
